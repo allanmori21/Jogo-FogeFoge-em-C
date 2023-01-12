fdc185fdc656a33f47f3d2bd5c72be4e71e7a3f5
allanmori21 <allanmori@hotmail.com>
2023-01-12 19:40:11 -0300

    Alocação dinâmica de memória no código

--- foge.c
+++ foge.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (){
-    char mapa[5][10+1];
+char** mapa;
+int linhas;
+int colunas;
 
+int main (){
     FILE* f;
     f = fopen ("mapa.txt", "r");
     if (f == 0){
         printf("Erro na leitura do mapa\n");
         exit(1);
     }
+
+    fscanf(f, "%d %d", &linhas, &colunas);
+    printf("linhas %d colunas %d\n", linhas, colunas);
     
+    mapa = malloc (sizeof(char*) * linhas);
+    for (int i = 0; i < linhas; i++){
+        mapa[i] = malloc(sizeof(char) * colunas+1);
+    }
+        
     for (int i = 0; i < 5; i++){
         fscanf (f, "%s", mapa[i]);
     }
@@ -21,4 +31,8 @@ int main (){
 
     fclose(f);
 
+    for (int i = 0; i < linhas; i++){
+        free(mapa[i]);
+    }
+    free(mapa);
 }
