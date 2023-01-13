4bb1ad71cebfb796cf637d13c2ebf88673ca6423
allanmori21 <allanmori@hotmail.com>
2023-01-13 13:37:43 -0300

    Adicionando ponteiros para structs no código

--- foge.h
+++ foge.h
@@ -1,15 +1,2 @@
-void liberamapa();
-void lemapa();
-void alocamapa();
-void imprimemapa ();
 int acabou ();
-void mover();
-
-struct mapa {
-    char** matriz;
-    int linhas;
-    int colunas;
-};
- 
-
-typedef struct mapa MAPA;
+void move(char direcao);
