b1b7c1308e889b9500d390d6b744cdb6ca6ae612
allanmori21 <allanmori@hotmail.com>
2023-01-16 15:46:09 -0300

    Adicionando recursividade no código

--- foge.c
+++ foge.c
@@ -1,83 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include "time.h"
 #include "foge.h"
 #include "mapa.h"
 
 MAPA m;
 POSICAO heroi;
-
-int praondefantasmavai (int xatual, int yatual, int* xdestino, int* ydestino){
-	int opcoes [4][2] = {
-		{ xatual, yatual+1},
-		{ xatual+1, yatual},
-		{ xatual, yatual-1},
-		{ xatual-1, yatual}	
-	};
-
-	srand (time(0));
-	for (int i = 0; i < 10; i++){
-		int posicao = rand() % 4;
-		 
-		if (podeandar(&m, FANTASMA, opcoes[posicao][0], opcoes[posicao][1])){
-			*xdestino = opcoes [posicao][0];
-			*ydestino = opcoes [posicao][1];
-		
-			return 1;
-		}
-	}
-
-	return 0;
-}
-
-void fantasmas (){
-	MAPA copia;
-
-	copiamapa(&copia, &m);
-
-	for (int i = 0; i < copia.linhas; i++){
-		for (int j = 0; j < copia.colunas; j++){
-			if (copia.matriz[i][j] == FANTASMA){
-
-				int xdestino;
-				int ydestino;
-
-				int encontrou = praondefantasmavai(i, j, &xdestino, &ydestino);
-
-				if (encontrou){
-					andandonomapa(&m, i, j, xdestino, ydestino);
-				}
-				
-			}
-		}
-	}
-	liberamapa(&copia);
-}
+int tempilula = 0;
 
 int acabou() {
-    POSICAO pos;
+	POSICAO pos;
 
-    int perdeu = !encontramapa(&m, &pos, HEROI);
-    int ganhou = !encontramapa(&m, &pos, FANTASMA);
+	int perdeu = !encontramapa(&m, &pos, HEROI);
+	int ganhou = !encontramapa(&m, &pos, FANTASMA);
 
-    return ganhou || perdeu;
-
+	return ganhou || perdeu;
+		
 }
 
-int ehdirecao(char direcao){
-    return direcao == 'a' ||
-           direcao == 'w' ||
-       	   direcao == 's' ||
-       	   direcao == 'd';
+int ehdirecao(char direcao) {
+	return
+		direcao == ESQUERDA || 
+		direcao == CIMA ||
+		direcao == BAIXO ||
+		direcao == DIREITA;
 }
 
 void move(char direcao) {
 
-    if (!ehdirecao(direcao))
-        return;
-        
-        int proximox = heroi.x;
-        int proximoy = heroi.y;
+	int proximox = heroi.x;
+	int proximoy = heroi.y;
 
 	switch(direcao) {
 		case ESQUERDA:
@@ -94,12 +46,72 @@ void move(char direcao) {
 			break;
 	}
 
-    if(!podeandar(&m, HEROI, proximox, proximoy))
-        return;
+	if(!podeandar(&m, HEROI, proximox, proximoy))
+		return;
 
-    andandonomapa (&m, heroi.x, heroi.y, proximox, proximoy);
-    heroi.x = proximox;
-    heroi.y = proximoy;
+	if(ehpersonagem(&m, PILULA, proximox, proximoy)) {
+		tempilula=1;
+	}
+
+	andanomapa(&m, heroi.x, heroi.y, proximox, proximoy);
+	heroi.x = proximox;
+	heroi.y = proximoy;
+}
+
+int praondefantasmavai(int xatual, int yatual, 
+	int* xdestino, int* ydestino) {
+
+	int opcoes[4][2] = { 
+		{ xatual   , yatual+1 }, 
+		{ xatual+1 , yatual   },  
+		{ xatual   , yatual-1 }, 
+		{ xatual-1 , yatual   }
+	};
+
+	srand(time(0));
+	for(int i = 0; i < 10; i++) {
+		int posicao = rand() % 4;
+
+		if(podeandar(&m, FANTASMA, opcoes[posicao][0], opcoes[posicao][1])) {
+			*xdestino = opcoes[posicao][0];
+			*ydestino = opcoes[posicao][1];
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+void fantasmas() {
+	MAPA copia;
+
+	copiamapa(&copia, &m);
+
+	for(int i = 0; i < copia.linhas; i++) {
+		for(int j = 0; j < copia.colunas; j++) {
+			if(copia.matriz[i][j] == FANTASMA) {
+
+				int xdestino;
+				int ydestino;
+
+				int encontrou = praondefantasmavai(i, j, &xdestino, &ydestino);
+
+				if(encontrou) {
+					andanomapa(&m, i, j, xdestino, ydestino);
+				}
+			}
+		}
+	}
+
+	liberamapa(&copia);
+}
+
+void explodepilula(int x, int y, int qtd) {
+
+	if(qtd == 0) return;
+
+	m.matriz[x][y+1] = VAZIO;
+	explodepilula(x, y+1, qtd-1);
 }
 
 int main() {
@@ -108,14 +120,16 @@ int main() {
 	encontramapa(&m, &heroi, HEROI);
 
 	do {
+		printf("Pílula: %s\n", (tempilula ? "SIM" : "NÃO"));
 		imprimemapa(&m);
 
 		char comando;
 		scanf(" %c", &comando);
 
-		move(comando);
+		if(ehdirecao(comando)) move(comando);
+		if(comando == BOMBA) explodepilula(heroi.x, heroi.y, 3);
 
-		fantasmas ();
+		fantasmas();
 
 	} while (!acabou());
 
